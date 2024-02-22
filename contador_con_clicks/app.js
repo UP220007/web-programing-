e0710ff7d4347edc0b83623deecdef7d35fa1da6
up220007 <up220007@alumnos.upa.edu.mx>
2024-02-21 18:13:52 -0600

    final code
    the conter of clicks eas finalised  with the js

--- contador_con_clicks/app.js
+++ contador_con_clicks/app.js
@@ -1,7 +1,38 @@
-let a = 1
-let b = 5
-let c = a - b
-alert(c)
+let contador = document.getElementById('container-clicks');
+let btnIncremento = document.querySelector('.incremento');
+let btnDecremento = document.querySelector('.decremento');
+let btnReset = document.querySelector('.reset')
+let count = 0;
+btnIncremento.addEventListener("click",function(){
+    count++;
+    contador.innerText = count;
+})
+//console.log(btnDecremento);
 
-let btn = document.getElementById("btn")
-console.log(btn)
+document.addEventListener("DOMContentLoaded", () =>{
+    contador .innerText = count;
+
+    btnIncremento.addEventListener("click",function(){
+        count++;
+        contador.innerText = count;
+    })
+
+    btnDecremento.addEventListener("click",function(){
+        if(count>=0){
+            count--;
+            if (count<0){
+                count=0;
+                contador.innerText = count;
+                alert("no puedes decrementar mas ya que el contador es cero")
+            }else{
+                contador.innerText = count;
+            }
+        } 
+        
+    })
+
+    btnReset.addEventListener("click",function(){
+        count=0;
+        contador.innerText = count;
+    })
+})
